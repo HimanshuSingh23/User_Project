a148a767e4be90b5080dfafa75f14e2a20aed64c
Himanshu Singh <himanshusingh792502gmail.com>
2024-07-02 13:30:56 +0530

    First Commit for this project

---
+++ UserProject/src/main/java/com/codeTesting/UserProject/UserProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codeTesting.UserProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProjectApplication.class, args);
+	}
+
+}
---
+++ UserProject/src/test/java/com/codeTesting/UserProject/UserProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeTesting.UserProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
